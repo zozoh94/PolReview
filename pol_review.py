46511abbc5ae86818c1ebe6e9c0edfec367e1cd7
Enzo Hamelin <enzo.hamelin@gmail.com>
2017-04-25 15:53:04 +0200

    Pip requirements

--- pol_review.py
+++ pol_review.py
@@ -6,38 +6,14 @@ import nltk
 import sys
 import textract
 import re
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QApplication
 
-class Window(QWidget):    
-    def __init__(self):
-        super().__init__()        
-        self.initUI()        
-        
-    def initUI(self):                       
-        self.resize(250, 150)
-        self.center()
-
-        self.setWindowTitle('Projets politiques des candidats à l\'élection présidentielle française de 2017')    
-        self.show()
-        
-        
-    def center(self):        
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-def parseProject(filename):
+def parse_project(filename):
     text = textract.process(project_directory + filename)
     text = text.decode('utf-8')
     return text
         
 if __name__ == '__main__':
     for candidate in candidates.values():
-        text = parseProject(candidate.get('file'))
+        text = parse_project(candidate.get('file'))
         text = nltk.word_tokenize(text, 'french')
         print(text)
-
-    #app = QApplication(sys.argv)
-    #ex = Window()
-    #sys.exit(app.exec_())
